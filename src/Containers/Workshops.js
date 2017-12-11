4edf44d6f33cf942eb3a0273f5d7490e86a23a6d
Carlos Marques <carlosmarques@arch-machine-broke.lan>
2017-12-11 03:03:48 +0000

    changed images

--- src/Containers/Workshops.js
+++ src/Containers/Workshops.js
@@ -10,6 +10,7 @@ import git from '../Images/git.png'
 import pov from '../Images/pov.jpg'
 import sail from '../Images/sail.png'
 import python from '../Images/python.png'
+import neecduino from '../Images/neecduino2013.png'
 
 class Workshops extends Component {
   render() {
@@ -19,7 +20,7 @@ class Workshops extends Component {
             tittle: "Programação de Arduino",
             desc: "",
             text: "Se achas que está na altura de tirar o pó ao teu Arduino e se queres dar outra utilidade que não a de pisa papéis, este é o workshop ideal para ti! Vais poder desenvolver um projeto controlado pelo teu arduino e no final ainda ficas com os componentes e a breadboard que utilizas. Download do Software: https://www.arduino.cc/en/Main/Software",
-            img: "",
+            img: matlab,
             links: {
                 facebook: "",
                 eventbrite: ""
@@ -39,7 +40,7 @@ class Workshops extends Component {
         {
             type: "coding",
             tittle: "GIT",
-            desc: "",
+            desc: "Vem aprender el giterino mon amigo",
             text: "Estás farto de projetos desorganizados? Queres programar e poder ter todos os teus ficheiros seguros e organizados num só sítio online? Queres entrar no universo da programação open source? O Git é um sistema de controlo de versões, com o qual podes desenvolver e armazenar projetos para os quais qualquer pessoa pode contribuir, não havendo asneira ao mudar a mesma parte de código. Se gostavas de aprender tudo sobre esta ferramenta importantíssima para o teu futuro na área da programação, não percas o workshop de Git trazido pelo NEEC! Não percas esta oportunidade de começar a programar como gente grande!",
             img: git,
             links: {
@@ -61,7 +62,7 @@ class Workshops extends Component {
         {
             type: "coding",
             tittle: "WebDev",
-            desc: "",
+            desc: "Vem aprender Webdeverino mon amegerino",
             text: "Sempre quiseste construir o teu próprio site, mas tens tanta informação à tua disposição que nem sabes por onde começar? Queres ter a tua página pessoal na Internet mas não sabes como iniciar esse projeto? O Workshop de Web Development do NEEC é a tua oportunidade para dar a volta à situação! Vem aprender tudo o que precisas para criar a tua página pessoal abordando os temas HTML, CSS, JavaScript e jQuery.",
             img: matlab,
             links: {
@@ -110,7 +111,7 @@ class Workshops extends Component {
             tittle: "NEECDuino",
             desc: "",
             text: "No primeiro ano do curso, chega aquela altura em que temos de mergulhar de cabeça no mundo da Programação e tornar-nos num verdadeiro hacker. Mas primeiro, convém aprender a instalar a principal ferramenta de trabalho, o Linux! Obviamente, o NEEC está aqui para ajudar nessa árdua tarefa!",
-            img: matlab,
+            img: neecduino,
             links: {
                 facebook: "",
                 eventbrite: ""
@@ -121,7 +122,7 @@ class Workshops extends Component {
             tittle: "POV",
             desc: "",
             text: "Estás farto de projetos desorganizados? Queres programar e poder ter todos os teus ficheiros seguros e organizados num só sítio online? Queres entrar no universo da programação open source? O Git é um sistema de controlo de versões, com o qual podes desenvolver e armazenar projetos para os quais qualquer pessoa pode contribuir, não havendo asneira ao mudar a mesma parte de código. Se gostavas de aprender tudo sobre esta ferramenta importantíssima para o teu futuro na área da programação, não percas o workshop de Git trazido pelo NEEC! Não percas esta oportunidade de começar a programar como gente grande!",
-            img: pov,
+            img: matlab,
             links: {
                 facebook: "",
                 eventbrite: ""
