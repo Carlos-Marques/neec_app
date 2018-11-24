6dc21a1436943afc5a849d9b297566410c2564f6
Carlos-Marques <carlosmarques.personal@gmail.com>
2018-11-24 02:00:45 +0000

    Removed workshop and event scroller from main page

--- src/App.js
+++ src/App.js
@@ -19,62 +19,36 @@ class App extends Component {
   render() {
     return (
       <div className="App" >
-    <PageHeader ><a line-height={100} href="about">Sites Úteis</a></PageHeader>
-    <Grid fluid>
-    <Row>
-    <Col xs >
-      <img src={require("./folder.png")} width={48} height={48}/>
-      <h4>Drive MEEC</h4>
-      <p>Gerimos a coleção de sebentas, apontamentos, testes e exames do nosso curso, disponíveis a qualquer aluno de MEEC.</p>
-      <a href="https://drive.neecist.org">drive.neecist.org</a>
-    </Col>
-    <Col xs >
-    <img src={require("./tecnico.png")} width={48} height={48}/>
-    <h4>Salas IST</h4>
-    <p>Perdido no IST? Não mais! Procura aqui a sala da qual nunca ouviste falar!</p>
-    <a href="https://salas.neecist.org">salas.neecist.org</a>
-  </Col>
-  <Col xs >
-  <img src={require("./work.png")} width={48} height={48}/>
-      <h4>IST Summer Internships</h4>
-      <p>Estágios de verão a que te poderás candidatar para enriquecer a tua experiência profissional.</p>
-      <a href="https://www.istsi.org">istsi.org</a>
-    </Col>
-    <Col xs >
-    <img src={require("./circuit.png")} width={48} height={48}/>
-      <h4>NEECLab</h4>
-      <p>Um laboratório de eletrónica para poderes desenvolver os teus projetos pessoais.</p>
-      <a href="https://neeclab.neecist.org">neeclab.neecist.org</a>
-    </Col>
-    </Row>
-      </Grid>
-    <PageHeader ><a href="about">Workshops</a></PageHeader>
-    <Grid fluid>
-    <Row>
-    <Col xs ><CardExampleWithAvatar /></Col>
-    <Col xs ><CardExampleWithAvatar /></Col>
-    <Col xs ><CardExampleWithAvatar /></Col>
-    </Row>
-    <Row>
-    <Col xs ><CardExampleWithAvatar /></Col>
-    <Col xs ><CardExampleWithAvatar /></Col>
-    <Col xs ><CardExampleWithAvatar /></Col>
-    </Row>
-      </Grid>
-      <PageHeader ><a href="about">Eventos</a></PageHeader>
-    <Grid fluid>
-    <Row>
-    <Col xs ><CardExampleWithAvatar /></Col>
-    <Col xs ><CardExampleWithAvatar /></Col>
-    <Col xs ><CardExampleWithAvatar /></Col>
-    </Row>
-    <Row>
-    <Col xs ><CardExampleWithAvatar /></Col>
-    <Col xs ><CardExampleWithAvatar /></Col>
-    <Col xs ><CardExampleWithAvatar /></Col>
-    </Row>
-      </Grid>
-    </div>
+        <PageHeader ><a line-height={100} href="about">Sites Úteis</a></PageHeader>
+        <Grid fluid>
+          <Row>
+            <Col xs >
+              <img src={require("./folder.png")} width={48} height={48} />
+              <h4>Drive MEEC</h4>
+              <p>Gerimos a coleção de sebentas, apontamentos, testes e exames do nosso curso, disponíveis a qualquer aluno de MEEC.</p>
+              <a href="https://drive.neecist.org">drive.neecist.org</a>
+            </Col>
+            <Col xs >
+              <img src={require("./tecnico.png")} width={48} height={48} />
+              <h4>Salas IST</h4>
+              <p>Perdido no IST? Não mais! Procura aqui a sala da qual nunca ouviste falar!</p>
+              <a href="https://salas.neecist.org">salas.neecist.org</a>
+            </Col>
+            <Col xs >
+              <img src={require("./work.png")} width={48} height={48} />
+              <h4>IST Summer Internships</h4>
+              <p>Estágios de verão a que te poderás candidatar para enriquecer a tua experiência profissional.</p>
+              <a href="https://www.istsi.org">istsi.org</a>
+            </Col>
+            <Col xs >
+              <img src={require("./circuit.png")} width={48} height={48} />
+              <h4>NEECLab</h4>
+              <p>Um laboratório de eletrónica para poderes desenvolver os teus projetos pessoais.</p>
+              <a href="https://neeclab.neecist.org">neeclab.neecist.org</a>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     );
   }
 }
